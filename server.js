144d9145b876f06f3223b9c29176b46e5d3c6588
Michael Chen <mjchen@Michaels-MacBook-Pro.local>
2019-09-09 09:39:50 -0700

    changed server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+const process = require('process');
+
+const port = process.env.PORT || 3000;
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -11,6 +15,6 @@ app.use(function(req, res, next) {
 var staticPath = path.join(__dirname, '/app');
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
 });
