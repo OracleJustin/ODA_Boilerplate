9ff010f86c99697b2d95fc6c17e1a8453884122f
OracleJustin <justin.ang@oracle.com>
2019-12-02 10:32:05 -0800

    port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 const process = require('process');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
