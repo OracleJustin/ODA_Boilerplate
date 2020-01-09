a5fae2a8f54e22a2e455b9f8a92f0d1ddd762b4b
OracleJustin <justin.ang@oracle.com>
2020-01-09 15:48:27 -0800

    need to refactor some code. Should destory & update app only when save changes is pressed, no need to validate before then

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,39 @@
+// Don't know what this does. Don't touch it
+
 !function (e, t, n, r) {
 	function s() { try { var e; if ((e = "string" == typeof this.response ? JSON.parse(this.response) : this.response).url) { var n = t.getElementsByTagName("script")[0], r = t.createElement("script"); r.async = !0, r.src = e.url, n.parentNode.insertBefore(r, n) } } catch (e) { } } var o, p, a, i = [], c = []; e[n] = { init: function () { o = arguments; var e = { then: function (t) { return c.push({ type: "t", next: t }), e }, catch: function (t) { return c.push({ type: "c", next: t }), e } }; return e }, on: function () { i.push(arguments) }, render: function () { p = arguments }, destroy: function () { a = arguments } }, e.__onWebMessengerHostReady__ = function (t) { if (delete e.__onWebMessengerHostReady__, e[n] = t, o) for (var r = t.init.apply(t, o), s = 0; s < c.length; s++) { var u = c[s]; r = "t" === u.type ? r.then(u.next) : r.catch(u.next) } p && t.render.apply(t, p), a && t.destroy.apply(t, a); for (s = 0; s < i.length; s++)t.on.apply(t, i[s]) }; var u = new XMLHttpRequest; u.addEventListener("load", s), u.open("GET", r + "/loader.json", !0), u.responseType = "json", u.send()
 }(window, document, "Bots", "http://flights-bot.herokuapp.com/bots-client-sdk-js");
+
+// Variables
+
+var color = "286090";
+var firstAppId = "";
+
+var defaultInit = {
+	businessName: 'Oracle, MCE',
+	businessIconUrl: '/images/oracle-o-logo.png',
+
+	customColors: {
+		brandColor: color,
+	},
+
+	customText: {
+		headerText: 'OMCE, How can we help?',
+		introductionText: 'Mobile Cloud Enterprise',
+	}
+}
+
+var defaultUser = {
+	"givenName": "John",
+	"surname": "Snow",
+	"email": "john.snow@winterfell.com",
+	"properties": {
+		"smoochCustomVariable1": "Lord",
+		"smoochCustomVariable2": "Commander"
+	}
+}
+
+// Chatbot Configuration Functions
 
 function loadAppId() {
 	var appId = window.localStorage.getItem("appId");
@@ -10,12 +43,10 @@ function loadAppId() {
 }
 
 function initApp(e) {
-	//e.preventDefault();
-	//console.log('Init Bots SDK');
 	var appId = window.localStorage.getItem("appId");
+	firstAppId = appId
 	initBots(appId)
 		.then(function () {
-			//console.log("init complete");
 			document.getElementById("loader").style.display = "none";
 		})
 		.catch(function (err) {
@@ -29,7 +60,7 @@ function saveAppId(e) {
 	let appId = document.getElementById("appId").value;
 	// console.log('Validate appId', appId);
 	// validate app id
-	initBots(appId)
+	initBots(appId, color)
 		.then(function () {
 			// console.log('AppId is valid');
 			window.localStorage.setItem("appId", appId);
@@ -66,6 +97,7 @@ function editAppId(e) {
 }
 
 function loadChat(e) {
+	console.log(Bots)
 	Bots.open()
 	document.getElementById("openChatButton").setAttribute("disabled", true)
 }
@@ -83,39 +115,24 @@ function clearChat(e) {
 }
 
 
-function initBots(appId) {
-	return Bots.init({
-		appId: appId,
-
-		businessName: 'Oracle, MCE',
-		businessIconUrl: '/images/oracle-o-logo.png',
-
-		customColors: {
-			brandColor: '286090',
-		},
-
-		customText: {
-			headerText: 'OMCE, How can we help?',
-			introductionText: 'Mobile Cloud Enterprise',
-		}
-	}).then(function (res) {
-		Bots.updateUser(
-			{
-				"givenName": "John",
-				"surname": "Snow",
-				"email": "john.snow@winterfell.com",
-				"properties": {
-					"smoochCustomVariable1": "Lord",
-					"smoochCustomVariable2": "Commander"
-				}
-			}
-		).catch(function (err) {
+function initBots(appId, color) {
+	defaultInit.appId = appId
+	defaultInit.color = color
+	return Bots.init(defaultInit).then(() => {
+		Bots.updateUser(defaultUser).catch(function (err) {
 			console.error(err);
 		});
 	});
 }
 
+// Administrative functions
+
 function saveChanges(e) {
+	let appId = document.getElementById("appId").value;
+	newInitValues = defaultInit
+	newInitValues.customColors.brandColor = color
+	newInitValues.appId = appId
+	console.log(newInitValues)
 	e.preventDefault()
 	var file = document.getElementById("getVal").files[0];
 	var reader = new FileReader();
@@ -130,10 +147,22 @@ function saveChanges(e) {
 				.style.display = "block";
 	}
 	if(file){
-			reader.readAsDataURL(file);
-	}else{
+		reader.readAsDataURL(file);
+	}
+
+	if (color !== "286090" || appId !== firstAppId){
+		console.log("oh no. Gotta reset alot of stuff now")
 	}
 }
+
+$('.theme-selector').on("click", function() {
+	colorArr = $(this).css("backgroundColor").split(", ");
+	partOne = colorArr[0].replace(/\D/g,'');
+	partTwo = colorArr[1].replace(/\D/g,'');
+	partThree = colorArr[2].replace(/\D/g,'');
+	color = fullColorHex(partOne, partTwo, partThree)
+	console.log(color);
+})
 
 function revertBackground(e) {
 	document.getElementById('body-elem')
@@ -145,3 +174,20 @@ function revertBackground(e) {
 	document.getElementById('return')
 		.style.display = "none";
 }
+
+// Color Functions
+
+var rgbToHex = function (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+var fullColorHex = function(r,g,b) {   
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red+green+blue;
+};
